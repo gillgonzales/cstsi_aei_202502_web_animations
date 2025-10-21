92e7dc3d20529b316368ade537466f0e93f0f879
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-10-21 17:07:30 -0300

    feat: exemplo animação SVGator como componente ReactJS

---
+++ aula05-tailwind-anime/exemplos-react/src/pages/Exemplo05SVGIcons/SVGatorCSSOnlyForReactJS.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SVGatorAnimation from "/assets/svgs/animated/svg_gator_animation_css.svg?url"
+
+const SVGatorCSSOnlyForReactJS = () => {
+  return (<>
+     <object data={SVGatorAnimation} width="300" height="200"></object> 
+  </>)
+}
+
+export {SVGatorCSSOnlyForReactJS}
